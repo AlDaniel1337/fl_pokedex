08dd751f7355ff59db6104eec80161c81bb22fef
Aldo <al-danmar@hotmail.com>
2025-10-16 15:03:35 -0600

    Modificado servicio de pokeapi

--- lib/infrastructure/services/poke_api.service.dart
+++ lib/infrastructure/services/poke_api.service.dart
@@ -4,11 +4,7 @@ import 'package:fl_pokedex/infrastructure/mappers/pokemon_model_to_entity.dart';
 import 'package:fl_pokedex/infrastructure/models/pokeapi_pokemon_response.dart';
 import 'package:fl_pokedex/infrastructure/models/pokeapi_pokemons_response.dart';
 
-import 'package:get/get.dart';
-
-/// PokeApiService _pokeApiService = Get.put(PokeApiService());
-
-class PokeApiService extends GetxController{
+class PokeApiService{
    
   final String _baseUrl = 'https://pokeapi.co/api/v2/';
   late DioPlugin _dioPlugin;
@@ -17,28 +13,36 @@ class PokeApiService extends GetxController{
     _dioPlugin = DioPlugin(_baseUrl);
   }   
 
-  RxList<Pokemon> pokemons = <Pokemon>[].obs;
-  
-  getPokemons() async {
+  final List<Pokemon> _pokemons = [];
+  Future<List<Pokemon>> getPokemons({int limit = 20, int offset = 0}) async {
 
-    if(pokemons.isNotEmpty) return;
+    _pokemons.clear();
 
-    pokemons.clear();
-    // Obtener datos de la API
-    final pokemonsResponse = await _dioPlugin.getResponse('pokemon?limit=10&offset=0');
-
-    // Pasar datos a un modelo
+    // 1. Obtener datos de la API principal
+    final pokemonsResponse = await _dioPlugin.getResponse('pokemon?limit=$limit&offset=$offset');
+    
+    // 2. Pasar datos a un modelo
     PokeapiPokemonsResponse model = PokeapiPokemonsResponse.fromJson(pokemonsResponse);
 
-    // Obtener detalles de un pokemon
-    model.results?.map( (pokemon) async {
+    // 3. Crear una lista de Futures para las llamadas a los detalles
+    final List<Future<Pokemon>> detailFutures = model.results!.map((pokemon) async {
+      // Await es necesario aquí para obtener el detalle ANTES de mapear al modelo/entidad
       final detail = await _dioPlugin.getResponse('pokemon/${pokemon.name}');
+      
       // Pasar detalles a un modelo
       final PokeapiPokemonResponse detailModel = PokeapiPokemonResponse.fromJson(detail);
-      // Pasar detalles a una entidad
-      final pokemonEntity = PokemonModelToEntity.fromModel(detailModel);
-      pokemons.add( pokemonEntity );
+      
+      // Pasar detalles a una entidad y devolver el Future<Pokemon>
+      return PokemonModelToEntity.fromModel(detailModel);
     }).toList();
+
+    // 4. Esperar a que TODOS los Futures de detalle se completen
+    final List<Pokemon> detailedPokemons = await Future.wait(detailFutures);
+
+    // 5. Agregar la lista completa a _pokemons
+    _pokemons.addAll(detailedPokemons);
+    
+    return _pokemons;
   }
   
 }
